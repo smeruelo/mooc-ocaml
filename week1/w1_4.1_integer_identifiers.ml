40d0f7f21372808822deea5e9691fc4864b69f26
Sonia Meruelo <smeruelo@gmail.com>
2018-10-03 14:43:36 +0200

    w1_4.1: use global definitions

--- week1/w1_4.1_integer_identifiers.ml
+++ week1/w1_4.1_integer_identifiers.ml
@@ -1,6 +1,6 @@
 let x = Random.int 9 + 1;;
 
-let x_power_2 = x * x in
-let x_power_4 = x_power_2  * x_power_2 in
-let x_power_8 = x_power_4 * x_power_4 in
+let x_power_2 = x * x;;
+let x_power_4 = x_power_2  * x_power_2;;
+let x_power_8 = x_power_4 * x_power_4;;
 x_power_8
