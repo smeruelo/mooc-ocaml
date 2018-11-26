056ffbe3e0d97a288540a9a852fcd34574d56cbc
Sonia Meruelo <smeruelo@gmail.com>
2018-11-26 23:24:20 +0100

    w3_3.1: corrections

--- week3/w3_3.1_symbolic_arithmetic.ml
+++ week3/w3_3.1_symbolic_arithmetic.ml
@@ -1,49 +1,35 @@
 type exp =
   | EInt of int
   | EAdd of exp * exp
-  | EMul of exp * exp
+  | EMul of exp * exp;;
 
 let example =
-  EAdd (EInt 1, EMul (EInt 2, EInt 3))
+  EAdd (EInt 1, EMul (EInt 2, EInt 3));;
 
-let my_example =
-  EAdd (EMul (EInt 2, EInt 2), EMul (EInt 3, EInt 3))
+let my_example = 
+  EAdd (EMul (EInt 2, EInt 2), EMul (EInt 3, EInt 3));;
 
 let rec eval e =
   match e with
-  | EInt i -> i
+  | EInt x ->  x
   | EAdd (e1, e2) -> eval e1 + eval e2
-  | EMul (e1, e2) -> eval e1 * eval e2
+  | EMul (e1, e2) -> eval e1 * eval e2;;
 
 let factorize e =
   match e with
-  | EAdd (EMul (a, b), (EMul (c, d))) ->
-    if a = c then
-      EMul (a, EAdd (b, d))
-    else if a = d then
-      EMul (a, EAdd (b, c))
-    else if b = c then
-      EMul (b, EAdd (a, d))
-    else if b = d then
-      EMul (b, EAdd (a, c))
-    else
-      e
-  | _ -> e
+  | EAdd (EMul (e1, e2), EMul (e3, e4)) when e1 = e3 -> EMul (e1, EAdd (e2, e4))
+  | _ -> e;;
 
 let expand e =
   match e with
-  | EMul (a, EAdd (b, c)) -> EAdd (EMul (a, b), (EMul (a, c)))
-  | _ -> e
+  | EMul (e1, EAdd (e2, e3)) -> EAdd (EMul (e1, e2), EMul (e1, e3))
+  | _ -> e;;
 
-(* rewritings are recursive! *)
 let simplify e =
   match e with
-  | EMul (_, (EInt 0)) -> EInt 0
-  | EMul ((EInt 0), _) -> EInt 0
-  | EMul (a, (EInt 1)) -> a
-  | EMul ((EInt 1), b) -> b
-  | EAdd (a, (EInt 0)) -> a
-  | EAdd ((EInt 0), b) -> b
-  | EMul (a, b) -> simplify (EMul (simplify a, simplify b))
-  | EAdd (a, b) -> simplify (EAdd (simplify a, simplify b))
-  | _ -> e
+  | EMul (e1, e2) when e1 = EInt 0 || e2 = EInt 0 -> EInt 0
+  | EMul (e, EInt 1) -> e
+  | EMul (EInt 1, e) -> e
+  | EAdd (e, EInt 0) -> e
+  | EAdd (EInt 0, e) -> e
+  | _ -> e;;
