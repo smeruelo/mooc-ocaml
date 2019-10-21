536a830ad48186d272926f363c083490fa1987d5
Sonia Meruelo <smeruelo@gmail.com>
2019-10-21 20:48:14 +0200

    w1_6.1: Use multiple_upto to check for prime

--- week1/w1_6.1_prime_numbers.ml
+++ week1/w1_6.1_prime_numbers.ml
@@ -1,5 +1,6 @@
 let rec gcd n m =
-  if m = 0 then n
+  if m = 0
+  then n
   else gcd m (n mod m);;
 
 let rec multiple_upto n r =
@@ -8,9 +9,6 @@ let rec multiple_upto n r =
   else multiple_upto n (r - 1);;
 
 let is_prime n =
-  let rec aux i =
-    if i < 2 then true
-    else if n mod i = 0 then false
-    else aux (i - 1) in
-  if n < 2 then false
-  else aux (integer_square_root n);;
+  if n = 1
+  then false
+  else not (multiple_upto n (int_of_float (sqrt (float_of_int n))));;
