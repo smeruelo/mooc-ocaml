abb19ef669271eb20ae90092ca89d6799fdc03d2
Sonia Meruelo <smeruelo@gmail.com>
2019-12-03 00:21:18 +0100

    w3_3.2: Minor changes

--- week3/w3_3.2_tries.ml
+++ week3/w3_3.2_tries.ml
@@ -1,8 +1,7 @@
 type trie = Trie of int option * char_to_children
 and char_to_children = (char * trie) list
 
-let empty =
-  Trie (None, [])
+let empty = Trie (None, [])
 
 let example =
   Trie (None,
@@ -17,40 +16,34 @@ let example =
 		         ('o', Trie (Some 7, []))]));
 	     ('A', Trie (Some 15, []))])
 
-let children_from_char m c =
-  let rec aux l =
-    match l with
+let children_from_char c2c c =
+  let rec aux = function
     | [] -> None
-    | (c', t)::tl ->
-      if c = c'
-      then Some t
-      else aux tl
-  in aux m
+    | (c', t) :: tl when c' = c -> Some t
+    | _ :: tl -> aux tl
+  in aux c2c
 
-let update_children m c t =
-  let rec aux m =
-    match m with
+let update_children c2c c t =
+  let rec aux = function
     | [] -> [(c, t)]
-    | (c', t')::tl ->
-      if c = c'
-      then (c, t)::tl
-      else (c', t')::(aux tl)
-  in aux m
+    | (c', t') :: tl when c' = c -> (c, t) :: tl
+    | hd :: tl -> hd :: (aux tl)
+  in aux c2c
 
-let lookup t s =
-  let rec aux idx (Trie (i, children)) =
-    if idx >= String.length s
-    then i
-    else match children_from_char children s.[idx] with
+let lookup t key =
+  let rec aux i (Trie (value, c2c)) =
+    if i >= String.length key
+    then value
+    else match children_from_char c2c key.[i] with
       | None -> None
-      | Some t -> aux (idx + 1) t
+      | Some t -> aux (i + 1) t
   in aux 0 t
 
-let insert t s k =
-  let rec aux idx (Trie (i, children)) =
-    if idx >= String.length s
-    then Trie (Some k, children)
-    else match children_from_char children s.[idx] with
-      | None -> Trie (i, update_children children s.[idx] (aux (idx + 1) empty))
-      | Some t -> Trie (i, update_children children s.[idx] (aux (idx + 1) t))
+let insert t key value =
+  let rec aux i (Trie (v, c2c)) =
+    if i >= String.length key
+    then Trie (Some value, c2c)
+    else match children_from_char c2c key.[i] with
+      | None -> Trie (v, update_children c2c key.[i] (aux (i + 1) empty))
+      | Some t -> Trie (v, update_children c2c key.[i] (aux (i + 1) t))
   in aux 0 t
