5fec102f15b49836bac2e1f38c70053391a8c966
Sonia Meruelo <smeruelo@gmail.com>
2019-12-09 12:16:03 +0100

    w3_6.1: small improvements

--- week3/w3_6.1_advanced_patterns.ml
+++ week3/w3_6.1_advanced_patterns.ml
@@ -1,16 +1,16 @@
 type e = EInt of int | EMul of e * e | EAdd of e * e
 
 let simplify = function
-  | EMul (EInt 1, e) | EMul (e, EInt 1) -> e
-  | EMul (EInt 0, e) | EMul (e, EInt 0) -> EInt 0
-  | EAdd (EInt 0, e) | EAdd (e, EInt 0) s| e -> e
+  | EMul (e, EInt 0) | EMul (EInt 0, e) -> EInt 0
+  | EMul (e, EInt 1) | EMul (EInt 1, e)
+  | EAdd (e, EInt 0) | EAdd (EInt 0, e)
+  | e -> e;;
 
 let only_small_lists = function
-  | ([] | [_] | [_;_]) as l -> l
-  | _ -> []
+  | ([_] | [_;_]) as l -> l
+  | _ -> [];;
 
 let rec no_consecutive_repetition = function
-  | [] -> []
-  | [x] -> [x]
   | x :: y :: ys when x = y -> no_consecutive_repetition (y :: ys)
-  | x :: y :: ys -> x :: (no_consecutive_repetition (y :: ys))
+  | x :: y :: ys -> x :: no_consecutive_repetition (y :: ys)
+  | l -> l;;
