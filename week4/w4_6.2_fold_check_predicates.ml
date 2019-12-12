29bb22d5eb2be8594823006d9e12a567527343c9
Sonia Meruelo <smeruelo@gmail.com>
2019-12-13 00:35:20 +0100

    w4_6_2: fix

--- week4/w4_6.2_fold_check_predicates.ml
+++ week4/w4_6.2_fold_check_predicates.ml
@@ -1,20 +1,16 @@
 let for_all p l =
-  List.fold_left (fun a h -> a && (p h)) true l
+  List.fold_left (fun acc x -> acc && p x) true l;;
 
 let exists p l =
-  List.fold_left (fun a h -> a || (p h)) false l
+  List.fold_left (fun acc x -> acc || p x) false l;;
 
-let sorted cmp l =
-  let f a h =
-    match a with
-    | Some a -> if cmp a h <= 0 then Some h else None
-    | None -> None
-  in
-  match List.fold_left f (Some (List.hd l)) l with
-  | Some _ -> true
-  | None -> false
-
-let my_cmp a b =
-  if a > b then 1
-  else if b > a then -1
-  else 0
+let sorted cmp = function
+  | [] -> true
+  | hd :: tl as l ->
+    let f acc x =
+      match acc with
+      | Some y -> if cmp y x <= 0 then Some x else None
+      | None -> None in
+    match List.fold_left f (Some (List.hd l)) l with
+    | Some _ -> true
+    | None -> false;;
