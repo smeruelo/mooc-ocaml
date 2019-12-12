18aeb8d6298a61be20abb6dbdd6a9d0645fe2244
Sonia Meruelo <smeruelo@gmail.com>
2019-12-12 23:47:41 +0100

    w4_6.1: 2019 version

--- week4/w4_6.1_fold_produce_lists.ml
+++ week4/w4_6.1_fold_produce_lists.ml
@@ -1,29 +1,19 @@
 let filter p l =
-  List.fold_right (fun x xs -> if p x then x::xs else xs) l []
-
-(* assumed that:
-  l is 'a list
-  f is (a' -> a' -> 'a)
-*)
-let reduce f neutral l =
-  List.fold_left f neutral l
-
-(* assumed that:
-   l is a' list
-   f is (a' -> 'b)
-*)
-let map f l =
-  List.fold_right (fun x xs -> (f x)::xs) l []
+  let f acc x =
+    if p x
+    then x :: acc
+    else acc in
+  List.fold_left f [] l;;
 
 let partition p l =
-  let f = fun x (xs_pos, xs_neg) ->
+  let f x (lpos, lneg) =
     if p x
-    then (x::xs_pos, xs_neg)
-    else (xs_pos, x::xs_neg) in
-  List.fold_right f l ([], [])
+    then (x :: lpos, lneg)
+    else (lpos, x :: lneg) in
+  List.fold_right f l ([], []);;
 
-let rec sort = function
+let rec sort l =
+  match l with
   | [] | [_] as l -> l
-  | h::r ->
-    let (lessers, greaters) = (partition (fun x -> x < h) r) in
-    (sort lessers) @ (h :: (sort greaters))
+  | hd :: tl -> let lpos, lneg = partition (fun x -> x <= hd) tl in
+    (sort lpos) @ (hd :: (sort lneg));;
