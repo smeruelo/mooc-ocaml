ad3107e3e9d9a372349f415e28a996ff1592fbe1
Sonia Meruelo <smeruelo@gmail.com>
2019-12-12 22:34:19 +0100

    w4_4.2: Complete it

--- week4/w4_4.2_small_arith_interpreter.ml
+++ week4/w4_4.2_small_arith_interpreter.ml
@@ -1,29 +1,33 @@
 type operation =
   | Op of string * operation * operation
-  | Value of int
+  | Value of int;;
 
-type env = (string * (int -> int -> int)) list
+type env = (string * (int -> int -> int)) list;;
 
-let rec lookup_function s = function
-  | [] -> (invalid_arg "lookup_function")
-  | (hd_s, hd_f) :: tl ->
-    if hd_s = s
-    then hd_f
-    else lookup_function s tl
+let rec lookup_function name = function
+  | [] -> invalid_arg "lookup_function"
+  | (str, f) :: tl when str == name -> f
+  | hd :: tl -> lookup_function name tl;;
 
 let add_function name f env =
-  (name, f) :: env
+  (name, f) :: env;;
 
-let my_env =
-  [("min", fun x y -> if x <= y then x else y)]
+let my_env = [
+  ("min", fun x y -> if y >= x then x else y);
+  ("add", fun x y -> x + y);
+  ("sub", fun x y -> x - y);
+  ("mul", fun x y -> x * y);
+  ("div", fun x y -> x / y)
+];;
 
 let rec compute env op =
   match op with
   | Value x -> x
   | Op (f_name, op1, op2) ->
-    let f = lookup_function f_name env
-    in f (compute env op1) (compute env op2)
+    let f = lookup_function f_name env in
+    f (compute env op1) (compute env op2);;
 
-(* su abuela sabrá que hacer en este apartado *)
-let rec compute_eff env = function _ ->
-  "Replace this string with your implementation"
+let rec compute_eff env = function
+  | Op (name, op1, op2) ->
+    lookup_function name env (compute_eff env op1) (compute_eff env op2)
+  | Value x -> x;;
