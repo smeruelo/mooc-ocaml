69c241458da02b8ffbb6728098cf560981b46770
Sonia Meruelo <smeruelo@gmail.com>
2019-12-14 00:53:44 +0100

    w5_2.1: Minor improvement

--- week5/w5_2.1_automatic_grader.ml
+++ week5/w5_2.1_automatic_grader.ml
@@ -1,32 +1,26 @@
 type report = message list
 and message = string * status
-and status = Successful | Failed
+and status = Successful | Failed;;
 
-type 'a result = Ok of 'a | Error of exn
+type 'a result = Ok of 'a | Error of exn;;
 
 
 let exec f x =
   try
     Ok (f x)
   with
-    e -> Error e
+    e -> Error e;;
 
 let compare user reference to_string =
   match user, reference with
-  | (Ok u, Ok r) ->
-    if u = r
-    then ("got correct value " ^ to_string u, Successful)
-    else ("got unexpected value " ^ to_string u, Failed)
-  | (Ok u, Error _) -> ("got unexpected value " ^ to_string u, Failed)
-  | (Error u, Ok _) -> ("got unexpected exception " ^ exn_to_string u, Failed)
-  | (Error u, Error r) ->
-    if u = r
-    then ("got correct exception " ^ exn_to_string u, Successful)
-    else ("got unexpected exception " ^ exn_to_string u, Failed)
+  | (Ok u, Ok r) when u = r -> ("got correct value " ^ to_string u, Successful)
+  | (Ok u, Ok _) | (Ok u, Error _) -> ("got unexpected value " ^ to_string u, Failed)
+  | (Error u, Error r) when u = r -> ("got correct exception " ^ exn_to_string u, Successful)
+  | (Error u, Error _) | (Error u, Ok _) -> ("got unexpected exception " ^ exn_to_string u, Failed);;
 
 let test user reference sample to_string =
   let rec aux = function
     | 0 -> []
-    | i -> let s = sample () in
-      (compare (exec user s) (exec reference s) to_string) :: aux (i - 1)
-  in aux 10
+    | i -> let test_input = sample () in
+      (compare (exec user test_input) (exec reference test_input) to_string) :: aux (i - 1)
+  in aux 10;;
