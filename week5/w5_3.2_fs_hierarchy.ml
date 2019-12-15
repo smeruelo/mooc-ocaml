951af5f42e4b8fed562e095c1a29677bb2f0089b
Sonia Meruelo <smeruelo@gmail.com>
2019-12-15 22:37:49 +0100

    w5_3.2: Fix

--- week5/w5_3.2_fs_hierarchy.ml
+++ week5/w5_3.2_fs_hierarchy.ml
@@ -1,104 +1,94 @@
-type filesystem =
-  (string * node) list
+type filesystem = (string * node) list
 and node =
   | File
   | Dir of filesystem
-  | Symlink of string list
+  | Symlink of string list;;
 
-
-let rec print_path path =
-  match path with
-  | [] -> Printf.printf ""
-  | hd :: [] -> Printf.printf "%s" hd
-  | hd :: tl -> Printf.printf "%s" (hd ^ "/"); print_path tl
+let rec print_path = function
+  | [] -> ()
+  | [x] -> (print_string x; print_newline ())
+  | hd :: tl -> print_string (hd ^ "/"); print_path tl;;
 
 let rec print_file lvl name =
-  match lvl with
-  | 0 -> Printf.printf "%s" (" " ^ name)
-  | i -> Printf.printf "%s" (" |"); print_file (i - 1) name
+  if lvl = 0
+  then (print_string name; print_newline ())
+  else (print_string "| "; print_file (lvl - 1) name);;
 
 let rec print_symlink lvl name path =
-  match lvl with
-  | 0 -> Printf.printf "%s" (" " ^ name ^ " -> "); print_path path
-  | i -> Printf.printf "%s" " |"; print_symlink (lvl - 1) name path
+  if lvl = 0
+  then (print_string (name ^ " -> "); print_path path)
+  else (print_string "| "; print_symlink (lvl - 1) name path);;
 
-let rec print_invalid lvl name path =
-  match lvl with
-  | 0 -> Printf.printf "%s" (" " ^ name ^ " -> INVALID")
-  | i -> Printf.printf "%s" " |"; print_invalid (lvl - 1) name path
+let rec print_invalid_symlink lvl name =
+  if lvl = 0
+  then (print_string (name ^ " -> "); print_string "INVALID"; print_newline ())
+  else (print_string "| "; print_invalid_symlink (lvl - 1) name);;
 
 let rec print_dir lvl name =
-  match lvl with
-  | 0 -> Printf.printf "%s" (" /" ^ name)
-  | i -> Printf.printf "%s" " |"; print_dir (lvl -1) name
+  if lvl = 0
+  then (print_string ("/" ^ name); print_newline ())
+  else (print_string "| "; print_dir (lvl - 1) name);;
 
 let print_filesystem root =
-  let rec print_filesystem lvl items =
-    match items with
+  let rec aux lvl = function
     | [] -> ()
-    | hd :: tl ->
-      begin
-        match hd with
-        | (name, File) -> print_file lvl name; print_newline ()
-        | (name, Dir fs) ->
-          print_dir lvl name; print_newline ();
-          print_filesystem (lvl + 1) fs
-        | (name, Symlink path) -> print_symlink lvl name path; print_newline ()
-      end;
-      print_filesystem lvl tl
-  in print_filesystem 0 root
+    | (name, node) :: tl -> match node with
+      | File -> (print_file lvl name; aux lvl tl)
+      | Dir fs -> (print_dir lvl name; aux (lvl + 1) fs; aux lvl tl)
+      | Symlink path -> (print_symlink lvl name path; aux lvl tl)
+  in
+  aux 0 root ;;
 
-let resolve sym path =
-  let rec aux abs rel =
-    match rel with
-    | [] -> List.rev abs
-    | ".." :: tl_rel -> aux (List.tl abs) tl_rel
-    | hd_rel :: tl_rel -> aux (hd_rel :: abs) tl_rel
-  in aux (List.tl (List.rev sym)) path
-
-let same_type_and_name ((name1 : string), node1) ((name2 : string), node2) =
-  match node1, node2 with
-  | (File, File) -> name1 = name2
-  | (Dir fs1, Dir fs2) -> name1 = name2
-  | (Symlink path1, Symlink path2) -> name1 = name2
-  | (_, _) -> false
+let rec resolve sym path =
+  let rec aux acc path =
+    match acc, path with
+    | _, [] -> List.rev acc
+    | [], ".." :: tl -> aux acc tl
+    | _, ".." :: tl -> aux (List.tl acc) tl
+    | _, hd :: tl -> aux (hd :: acc) tl
+  in
+  aux (List.tl (List.rev sym)) path;;
 
 let rec file_exists root path =
+  let node_matches ((node1_name : string), node1) ((node2_name : string), node2) =
+    match node1, node2 with
+    | (File, File) | (Dir _, Dir _) -> node1_name = node2_name
+    | (_, _) -> false
+  in
   match path with
-  | last :: [] ->
+  | [] -> false
+  | [target] ->
     begin
       try
-        let (_, _) = List.find (same_type_and_name (last, File)) root in
+        let _ = List.find (node_matches (target, File)) root in
         true
       with
         Not_found -> false
     end
-  | current :: tl ->
+  | hd :: tl ->
     begin
       try
-        let (name, Dir dir) = List.find (same_type_and_name (current, Dir [])) root in
-        file_exists dir tl
+        match List.find (node_matches (hd, Dir [])) root with
+        | target, Dir fs -> file_exists fs (List.tl path)
+        | _, _ -> false
       with
         Not_found -> false
-    end
+    end;;
 
 let print_filesystem root =
-  let rec print_filesystem current_path items =
-    match items with
+  let rec aux abs_path lvl = function
     | [] -> ()
-    | hd :: tl ->
-      begin
-        let lvl = List.length current_path in
-        match hd with
-        | (name, File) ->
-          print_file lvl name; print_newline ()
-        | (name, Dir fs) ->
-          print_dir lvl name; print_newline ();
-          print_filesystem (current_path @ [name]) fs
-        | (name, Symlink path) ->
-          if file_exists root (resolve (current_path @ [name]) path)
-          then begin print_symlink lvl name path; print_newline () end
-          else begin print_invalid lvl name path; print_newline () end
-      end;
-      print_filesystem current_path tl
-  in print_filesystem [] root
+    | (name, node) :: tl -> match node with
+      | File -> (print_file lvl name;
+                 aux abs_path lvl tl)
+      | Dir fs -> (print_dir lvl name;
+                   aux (name :: abs_path) (lvl + 1) fs;
+                   aux abs_path lvl tl)
+      | Symlink path ->
+        if file_exists root (resolve (List.rev (name :: abs_path)) path) then
+          (print_symlink lvl name path;
+           aux abs_path lvl tl)
+        else
+          (print_invalid_symlink lvl name;
+           aux abs_path lvl tl)
+  in aux [] 0 root ;;
