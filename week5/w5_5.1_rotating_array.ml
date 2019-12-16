393f4eabe8ebf371c342f3106c89eb5cb671d2db
Sonia Meruelo <smeruelo@gmail.com>
2019-12-17 00:08:11 +0100

    w5_5.1: fix

--- week5/w5_5.1_rotating_array.ml
+++ week5/w5_5.1_rotating_array.ml
@@ -1,18 +1,19 @@
 let rotate a =
-  print_char '*';
   let n = Array.length a in
-  if n > 1 then
+  match n with
+  | 0 -> ()
+  | _ ->
     let v = a.(0) in
     for i = 0 to n-2 do
       a.(i) <- a.(i+1)
     done;
-    a.(n-1)<-v
+    a.(n-1)<-v ;;
 
-
-let rotate_by a n =
-  let l = Array.length a in
-  let real_n = n mod l in
-  if real_n != 0 then
-    for i = 1 to real_n do
+let rotate_by a k =
+  let n = Array.length a in
+  match n with
+  | 0 -> ()
+  | n ->
+    for i = 1 to (n + k) mod n do
       rotate a
     done
