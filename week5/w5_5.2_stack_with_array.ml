2d092041f367e7d770fed6b9ad2694e7a784e2af
Sonia Meruelo <smeruelo@gmail.com>
2019-12-17 11:45:39 +0100

    w5_5.2: 2019s solution

--- week5/w5_5.2_stack_with_array.ml
+++ week5/w5_5.2_stack_with_array.ml
@@ -1,43 +1,29 @@
-type stack = int array
-exception Full
-exception Empty
+type stack = int array;;
+exception Full;;
+exception Empty;;
 
 let create size =
-  let a = Array.init (size + 1) (fun _ -> 0) in a
-
-let top buf =
-  buf.(0)
-
-let is_full buf =
-  top buf = (Array.length buf) - 1
-
-let is_empty buf =
-  top buf = 0
-
-let print_stack buf =
-  for i = top buf downto 1 do
-    print_int i; print_newline ()
-  done
+  let a = Array.make (size + 1) 0 in
+  a;;
 
 let push buf elt =
-  if is_full buf then
+  if buf.(0) = ((Array.length buf) - 1) then
     raise Full
   else
     begin
-      buf.(0) <- (top buf) + 1;
-      buf.(top buf) <- elt
-    end
+      buf.(buf.(0) + 1) <- elt;
+      buf.(0) <- buf.(0) + 1
+    end;;
 
 let append buf arr =
-  for i = Array.length(arr) - 1 downto 0 do
+  for i = (Array.length arr) - 1 downto 0 do
     push buf arr.(i)
-  done
+  done;;
 
 let pop buf =
-  if is_empty buf then
+  if buf.(0) = 0 then
     raise Empty
   else
-    begin
-      buf.(0) <- (top buf) - 1;
-      buf.((top buf) + 1)
-    end
+    let top = buf.(buf.(0)) in
+    buf.(0) <- buf.(0) - 1;
+    top;;
